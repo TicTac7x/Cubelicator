b447e4decad37721e8d717f4e90a33a5c17e75c8
TicTac7x <tictac7x@gmail.com>
2026-06-09 22:11:41 +0300

    WinUI3 and titlebar used

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Cubelicator;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(TitleBar);
+        TitleBar.Title = Strings.AppName;
+        Root.ActualThemeChanged += (_, _) => UpdateTitleBarTheme();
+    }
+
+    private void UpdateTitleBarTheme()
+    {
+        var titleBar = AppWindow.TitleBar;
+
+        bool isDark =
+            Root.ActualTheme == ElementTheme.Dark;
+
+        titleBar.ButtonForegroundColor = isDark
+            ? Microsoft.UI.Colors.White
+            : Microsoft.UI.Colors.Black;
+    }
+}
